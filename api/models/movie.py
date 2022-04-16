f6e958d3eeb0d83eb1fdab8a84baccdd698ddc29
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-04-16 21:48:34 +0300

    [FIX]: movie models

--- api/models/movie.py
+++ api/models/movie.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from api import db
+from api.db import db
 
 
 class Genres(Enum):
@@ -15,7 +15,7 @@ class Film(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     genre = db.Column(db.Enum(Genres), default=None)
-    director = db.Column(db.Integer, db.ForeignKey('director.id'))
+    director = db.Column(db.Integer, db.ForeignKey('director.id'), default=None)
 
     def __repr__(self):
         return self.title
@@ -29,15 +29,24 @@ class Film(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_by_name(cls, name):
-        return cls.query.get_or_404(name)
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id)
 
 
 class Director(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     surname = db.Column(db.String(255), nullable=False)
-    film = db.Column(db.relationship('Film', backref='director'))
+    film = db.relationship('Film')
 
     def __repr__(self):
         return self.name + ' ' + self.surname
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
